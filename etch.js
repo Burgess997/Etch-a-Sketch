1a2ae1561e6eb64a9f6f42759894cfbecbb6efb9
Burgess997 <c13burgess@gmail.com>
2022-10-24 21:46:40 +0100

    Made Box Event Listeners Work While Dragging

--- etch.js
+++ etch.js
@@ -1,25 +1,34 @@
-let etch = document.querySelector(".etch")
-let box = document.createElement("div")
+let etch = document.querySelector(".etch");
+let box = document.createElement("div");
+var mouseDown = 0;
 
-createGrid(16)
+document.body.onmousedown = function () {
+  ++mouseDown;
+};
+document.body.onmouseup = function () {
+  --mouseDown;
+};
 
-function createGrid(gridSize){
-    box.id = "grid-box";
-    box.style.width = (500 / gridSize).toString().concat("px");
-    box.style.height = (500 / gridSize).toString().concat("px")
-    box.addEventListener('click', (box) => {
-        console.log("S")
-    })
-    for (let i = 0; i < gridSize * gridSize; i++){
-        let boxClone = box.cloneNode()
-        etch.appendChild(boxClone);
-        boxClone.addEventListener('click', () => {
-            boxClicked(boxClone)
-        })
-    }
+createGrid(16);
+
+function createGrid(gridSize) {
+  box.id = "grid-box";
+  box.style.width = (500 / gridSize).toString().concat("px");
+  box.style.height = (500 / gridSize).toString().concat("px");
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let boxClone = box.cloneNode();
+    etch.appendChild(boxClone);
+    boxClone.addEventListener("mouseover", (box) => {
+      if (mouseDown == 1) {
+        boxClicked(boxClone, box);
+      }
+    });
+    boxClone.addEventListener("mousedown", (box) => {
+        boxClicked(boxClone, box);
+    });
+  }
 }
 
-function boxClicked(box) {
-    box.style.backgroundColor = "red"
-    console.log(box)
+function boxClicked(box, eventData) {
+  box.style.backgroundColor = "red";
 }
