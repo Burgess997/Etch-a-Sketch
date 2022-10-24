58da9aed5741d4a97f11d61db0acae254d6ba1c4
Burgess997 <c13burgess@gmail.com>
2022-10-24 21:02:41 +0100

    Add Create Grid Function

--- etch.js
+++ etch.js
@@ -1,1 +1,14 @@
 let etch = document.querySelector(".etch")
+let box = document.createElement("div")
+
+createGrid(16)
+
+function createGrid(gridSize){
+    box.id = "grid-box";
+    box.style.width = (500 / gridSize).toString().concat("px");
+    box.style.height = (500 / gridSize).toString().concat("px")
+
+    for (let i = 0; i < gridSize * gridSize; i++){
+        etch.appendChild(box.cloneNode());
+    }
+}
