80d05705bfb513d00c1d0f7c42c4d6bcbb804824
Burgess997 <c13burgess@gmail.com>
2022-10-24 21:07:49 +0100

    Add Box Clicked Function

--- etch.js
+++ etch.js
@@ -7,8 +7,18 @@ function createGrid(gridSize){
     box.id = "grid-box";
     box.style.width = (500 / gridSize).toString().concat("px");
     box.style.height = (500 / gridSize).toString().concat("px")
-
+    box.addEventListener('click', (box) => {
+        console.log("S")
+    })
     for (let i = 0; i < gridSize * gridSize; i++){
-        etch.appendChild(box.cloneNode());
+        let boxClone = box.cloneNode()
+        etch.appendChild(boxClone);
+        boxClone.addEventListener('click', (box) => {
+            boxClicked(box)
+        })
     }
+}
+
+function boxClicked() {
+
 }
